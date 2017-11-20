bc246155ef1fc6834740586a18ea3e2ca6f6da28
drao9 <divyani.rao95@gmail.com>
2017-11-19 19:34:06 -0500

    done

--- label.py
+++ label.py
@@ -2,7 +2,7 @@ import sys
 print(sys.argv[1])
 with open('sorted_emails.txt','r') as fp:
 	emails = fp.readlines()
-	with open('abrelsfo_labeled.txt', 'a') as f:
+	with open('drao_labeled.txt', 'a') as f:
 		for count, line in enumerate(emails[int(sys.argv[1]): int(sys.argv[2])]):
 			print(count + int(sys.argv[1]))
 			print(line.strip('\n').strip(' '))
